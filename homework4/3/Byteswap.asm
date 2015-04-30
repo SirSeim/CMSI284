b684dfd081b04050b3efb99a89879ae1246bcadd
Trixie Roque <troque@lion.lmu.edu>
2015-04-29 18:58:41 -0700

    Here's #3

--- homework4/3/Byteswap.asm
+++ homework4/3/Byteswap.asm
@@ -1,20 +1,34 @@
-        global  byteswap
+ global  byteswap
         section .text
 
+;byteswap:
+
+;        mov     rsi, rdi
+;        xor     rax, rax
+
+;leep:
+;        mov     rcx, rsi
+;        and     rcx, 0xFF000000
+;        or      rax, rcx
+;        shr     rax, 8
+;        shl     rsi, 8
+;        cmp     rcx, 0
+;        jz      done
+;        jmp     leep
+
+;done:
+;        ret
+
+;Trixie wrote this part
 byteswap:
+        mov     dl, [rdi]
+        mov     al, [rdi+1]
+        mov     cl, [rdi+2]
+        mov     bl, [rdi+3]
 
-        mov     rsi, rdi
-        xor     rdi, rdi
+        mov     [rdi], bl
+        mov     [rdi+1], cl
+        mov     [rdi+2], al
+        mov     [rdi+3], dl
 
-leep:
-        mov     rcx, rsi
-        and     rcx, 0xFF000000
-        or      rdi, rcx
-        shr     rdi, 8
-        shl     rsi, 8
-        cmp     rcx, 0
-        jz      done
-        jmp     leep
-
-done:
         ret
